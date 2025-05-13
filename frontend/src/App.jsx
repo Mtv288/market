f8e5644f4d88d45c574d9453d9d03e02b3392998
bazylevnik0 <bazylevnik0@gmail.com>
2025-05-13 22:50:30 +0300

    Chakra добавлены настройки

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg'
+import chakraLogo from './assets/chakra.png'
 import './App.css'
+import { Container, Link, Image } from "@chakra-ui/react"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Маркет</h1>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+     <Container>
+        <Link href="https://vite.dev" target="_blank">
+          <Image src={viteLogo} alt="Vite logo" />
+        </Link>
+        <Link href="https://react.dev" target="_blank">
+          <Image src={reactLogo} alt="React logo" />
+        </Link> <br />
+        <Link href="https://chakra-ui.com/">
+          <Image src={chakraLogo} alt="Chakra logo" />
+        </Link>
+      </Container>
     </>
   )
 }

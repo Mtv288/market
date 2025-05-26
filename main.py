78c2d6c7c706c702a1e35d8aa2b062021fb3328f
Mtv288 <meremkulovtv@gmail.com>
2025-05-26 13:32:36 +0300

    clear_directory_frontend

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
 from backend.models.db_main import create_database, create_tables
 from backend.routers import router
 
@@ -12,11 +20,22 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+# Абсолютный путь к папке static
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "frontend", "login_registration", "static")
+
+# Подключение статики
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
+# Путь к шаблонам (если они лежат рядом с static)
+TEMPLATES_DIR = os.path.join(BASE_DIR, "frontend", "login_registration", "templates")
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
+
 app.include_router(router)
 
-@app.get("/")
-async def read_root():
-    return {"status": "ok"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
